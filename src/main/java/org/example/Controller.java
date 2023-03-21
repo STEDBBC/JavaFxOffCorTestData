591ffdcac6fd35c91945471cad92edff34f5caef
v.makareko <v.makarenko@niaepnn.ru>
2023-03-21 14:36:35 +0300

    добавил ограниченипе на ввод в поле количество писем (только числа можно вводить)

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
@@ -23,6 +25,9 @@ public class Controller {
 
     @FXML
     private ComboBox<String> organizationComboBox;
+    @FXML
+    private TextField lettersAmountTextField;
+
 
     @FXML
     private DatePicker letterDatePicker;
@@ -35,6 +40,7 @@ public class Controller {
         userNameComboBox.getItems().addAll("ec_user1", "migration_user", "oo_user1");
         organizationComboBox.getItems().addAll("JSC EC ASE", "JSC ASE", "Nuclear Power Plant Authority");
     }
+
     @FXML
     private void chooseDirectory() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
@@ -54,29 +60,50 @@ public class Controller {
             return;
         }
 
+        int lettersAmount;
+        try {
+            lettersAmount = Integer.parseInt(lettersAmountTextField.getText());
+        } catch (NumberFormatException e) {
+            showAlert("Введите корректное количество писем");
+            return;
+        }
+
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("M/d/yyyy h:mm:ss a");
         String formattedDate = letterDatePicker.getValue().atStartOfDay().format(dateFormatter);
-
-        String script = "set context person creator;\n" +
-                "start  transaction;\n" +
-                "add bus IMS_PM_Letter TestPmLetter6 0 policy 'IMS_PM_ContractorsLetter' Description '999' IMS_Code 'ED-ASEM-NPPA-CM-000125' Originated '" + formattedDate + "' IMS_RegistrationNumber '322' Title 'titleTest' owner '" + userNameComboBox.getValue() + "' Originator '" + userNameComboBox.getValue() + "' project 'JSC EC ASE CS' Organization '" + organizationComboBox.getValue() + "' current 'ReadyForSending' IMS;";
 
         File outputFile = new File(chosenDirectory, "output_script.txt");
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
-            writer.write(script);
+            for (int i = 1; i <= lettersAmount; i++) {
+                String script = "set context person creator;\n" +
+                        "start  transaction;\n" +
+                        "add bus IMS_PM_Letter TestPmLetter" + i + " 0 policy 'IMS_PM_ContractorsLetter' Description '999' IMS_Code 'ED-ASEM-NPPA-CM-000125' Originated '"
+                        + formattedDate + "' IMS_RegistrationNumber '322' Title 'titleTest"
+                        + i + "' owner '" + userNameComboBox.getValue() + "' Originator '"
+                        + userNameComboBox.getValue() + "' project 'JSC EC ASE CS' Organization '"
+                        + organizationComboBox.getValue() + "' current 'ReadyForSending' IMS_CorrespondenceSubject 'subject text' IMS_RegistrationDate '"
+                        + formattedDate + "';\n";
+                writer.write(script);
+            }
             showAlert("Файл успешно сохранен");
         } catch (IOException e) {
             showAlert("Ошибка при сохранении файла");
             e.printStackTrace();
         }
     }
+
     private void showAlert(String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Information");
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
+    }
+    @FXML
+    private void handleNumberInput(KeyEvent event) {
+        if (!event.getCharacter().matches("\\d")) {
+            event.consume();
+        }
     }
 }
 
